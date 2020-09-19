0bbf31b8ebc6eb90ed10430b89c30e19e482d3f6
Akshat Joshi <39386084+AkshatJoshi2000@users.noreply.github.com>
2020-09-19 13:27:17 +0530

    Create news.py
    Function for news headlines

---
+++ news.py
@@ -0,0 +1,29 @@
+import requests
+import datetime
+
+def everything_news(req):
+    now = datetime.datetime.now()
+    a = str(now.year)
+    b = str(now.month)
+    c = str(now.day-1)
+    p = str(a+"-"+b+"-"+c)
+    url = ('https://newsapi.org/v2/everything?'
+                            'q='+req+
+                            '&from='+p+
+                            '&sortBy=popularity&'
+                            'apiKey=b5898a93c0704b83a085d7e1b2f408a4')
+    response = requests.get(url)
+    z = response.json()
+    H1 =  z['articles'][0]['title']
+    H2 =  z['articles'][1]['title']
+    H3 =  z['articles'][2]['title']
+    H4 =  z['articles'][3]['title']
+    H5 =  z['articles'][4]['title']
+    answer = """
+    1. {}
+    2. {}
+    3. {}
+    4. {}
+    5. {}""".format(H1,H2,H3,H4,H5)
+
+    return answer
