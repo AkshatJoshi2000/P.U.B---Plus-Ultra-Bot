fc065d1d8678e13f6651dee7132854cc8e3f4ea4
AkshatJoshi2000 <akshatjoshi2000@gmail.com>
2020-09-23 02:13:04 +0530

    weather function

---
+++ weather.py
@@ -0,0 +1,15 @@
+import requests
+
+
+def weather_res(city, country):
+    # api_location = "http://ip-api.com/json/?fields=61439"
+
+    # json_data_1 = requests.get(api_location).json()
+    # city = json_data_1['city']
+    # country = json_data_1['country']
+
+    api_weather = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + country + "&units=metric&APPID=<API ID>"
+
+    json_data_2  = requests.get(api_weather).json()
+    return json_data_2
+    
