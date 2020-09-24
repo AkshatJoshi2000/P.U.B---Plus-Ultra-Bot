b5af7c548474707f29094e63b695a3ec4fb01c11
Abubfatmi <71751607+Abubfatmi@users.noreply.github.com>
2020-09-25 01:02:20 +0530

    Create barney.py
    barney quotes function

---
+++ barney.py
@@ -0,0 +1,27 @@
+import bs4
+import requests
+import time
+import json
+from urllib.request import Request,urlopen
+import numpy
+def barney():
+    url = "https://www.scarymommy.com/barney-stinson-quotes/"
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    p = numpy.random.randint(20,size=1)
+    q = p[0]
+    webpage = urlopen(req).read()
+    article_text = ''
+    soup = bs4.BeautifulSoup(webpage, 'html.parser')
+    title = soup.find('div',attrs = {'class' : 'entry-content'})
+    ol = title.find('ol')
+    z=[]
+    for li in ol:
+
+        z.append(li)
+
+    p = str(z[q])
+    b =p[4:-5]
+    return b
+
+barney()
+
