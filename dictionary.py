ab7c08e53f0a9610a0625f94204a5c088c2aaf06
Akshat Joshi <39386084+AkshatJoshi2000@users.noreply.github.com>
2020-10-01 21:21:51 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -8,7 +8,7 @@ def Dictionary(term):
 
     headers = {
         'x-rapidapi-host': "mashape-community-urban-dictionary.p.rapidapi.com",
-        'x-rapidapi-key': "91d6012593msh027f1b4da0b41d0p1aeae0jsn95b311087663"
+        'x-rapidapi-key': "<API- KEY>"
         }
 
     response = requests.request("GET", url, headers=headers, params=querystring)
