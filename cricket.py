fadf3504397ec2a2acd2b89d52c49d65f3b8157a
Ish Kumar <47569653+ish-kumar@users.noreply.github.com>
2020-10-07 23:50:52 +0530

    Update cricket.py

--- cricket.py
+++ cricket.py
@@ -16,7 +16,7 @@ def cricket_live():
                   soup = bs4.BeautifulSoup(webpage, 'html.parser')
                   top_row=soup.find_all('div', attrs= {"class":"cb-mtch-lst cb-col cb-col-100 cb-tms-itm"})  
                   for match in top_row:
-                        teams = match.div.h3.text
+                        teams = match.div.h3.text[1:-2]
                         print(teams)
                         block = match.find('div', attrs = {"class":"cb-scr-wll-chvrn cb-lv-scrs-col"})
                         scores = block.find_all('div')
@@ -25,12 +25,21 @@ def cricket_live():
                               scores[5].text = " - Yet To Bat"
                         score_2 = scores[4].text + ' : ' + scores[5].text
                         
-                  
-                        status = block.find('div', attrs = {"class":"cb-text-live"}).text
-                        res = """
-                              *{}*
-                              {}  {}
-                              {}""".format(teams, score_1, score_2, status)
-                        return(res)
-            except:
-                  return "No ongoing matches" 
+                        try:
+                            status = block.find('div', attrs = {"class":"cb-text-live"}).text
+                            res = """
+                                *{}*
+                                {}  {}
+                                {}""".format(teams, score_1, score_2, status)
+                            return(res)    
+                        except:
+                            status = block.find('div', attrs = {"class":"cb-text-complete"}).text
+                            res = """
+                                *{}*
+                                {}  {}
+                                {}""".format(teams, score_1, score_2, status)
+                            return(res) 
+
+            except :
+                return "No Ongoing Matches"                              
+
