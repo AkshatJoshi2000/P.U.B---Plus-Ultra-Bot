47783b298e7be07cdd4e2f10820db18cac101fea
Ish Kumar <47569653+ish-kumar@users.noreply.github.com>
2020-10-08 00:30:52 +0530

    added h2h

--- football.py
+++ football.py
@@ -37,5 +37,32 @@ def football_live():
                         {}""".format(title, teams, score, minutes)
                   print(res)
     
+def football_h2h(TeamA, TeamB):
+    TeamA = TeamA.replace(' ', '-')
+    TeamB = TeamB.replace(' ', '-')
+    import bs4
 
+    import requests
+    import time
+    import json
+    import numpy as np
+    url = "https://www.fctables.com/h2h/%s/%s/"%(TeamA, TeamB)
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        webpage = response.content
+        final_list = []
+        soup = bs4.BeautifulSoup(webpage, 'html.parser')
+        top_row=soup.find('div', attrs= {"class":"game_name"})    
+        team_a = top_row.div.a.span.text
+        team_b = top_row.find('div', attrs = {"class":"gnbox away"}).a.span.text
+        scores_main = soup.find('div', attrs= {"class":"score"})
+        score = scores_main.ul.li.h3.text
+        date_time = scores_main.ul.li.p.text[:-6]
+        teams = '%s vs %s'%(team_a, team_b)
+        res="""
+            *{}*
+            {}
+            {}""".format(teams, score, date_time)
+        return(res)
 football_live()
